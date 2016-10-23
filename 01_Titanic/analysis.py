0781276dc4cdd17a089bd36b3592200057f484e7
Muhammad Rehman <thatsocliche@hotmail.com>
2016-10-23 00:01:26 -0400

    Imputing the Age column

--- 01_Titanic/analysis.py
+++ 01_Titanic/analysis.py
@@ -3,4 +3,8 @@ import pandas as pd
 trainingSet = pd.read_csv('train.csv')
 
 print(trainingSet.head(5))
+# print(trainingSet.describe())
+
+#Imputing our dataframe
+trainingSet["Age"]=trainingSet["Age"].fillna(trainingSet["Age"].median())
 print(trainingSet.describe())
