4c14c58158d0fca20bdfcb9c571a301ee11cce22
Muhammad Rehman <thatsocliche@hotmail.com>
2016-10-23 00:46:22 -0400

    Linear regression using scilearn and using Cross-Validation k-folds

--- 01_Titanic/analysis.py
+++ 01_Titanic/analysis.py
@@ -1,9 +1,12 @@
 import pandas as pd 
+import sklearn
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import KFold
 
 trainingSet = pd.read_csv('train.csv')
 
-print(trainingSet.head(5))
-# print(trainingSet.describe())
+#Columns used to make our predictions
+features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
 
 #Imputing our dataframe
 trainingSet["Age"]=trainingSet["Age"].fillna(trainingSet["Age"].median())
@@ -21,14 +24,25 @@ trainingSet.loc[trainingSet["Embarked"] == 'S', "Embarked"] = 0
 trainingSet["Embarked"][trainingSet["Embarked"] == 'C'] = 1
 trainingSet["Embarked"][trainingSet["Embarked"] == 'Q'] = 2
 
-# print(trainingSet["Embarked"])
 
 
-#Using Linear Regression based on Age to predict
-m = -2 
-b = 20
-trainingSet["LinPredictions"] = m*trainingSet["Age"] + b
-trainingSet["LinPredictions"][trainingSet["LinPredictions"] < 0] = int(0)
-trainingSet.loc[trainingSet["LinPredictions"] > 0] = int(1)
+#Using Linear Regression 
+algorithm = LinearRegression()
 
-print(trainingSet["LinPredictions"])
+#Splitting our data 
+#Returns the row indices for the training folds and the test folds
+kf = KFold(trainingSet.shape[0], n_folds=3, random_state=1)
+
+predictions = []
+for trainIndices,testIndices in kf:
+	#Subset of the dataframe with only training indices and features
+	trainPredictors = trainingSet[features].iloc[trainIndices,:]
+	#Setting our train training target 
+	trainingTarget = trainingSet["Survived"].iloc[trainIndices]
+	#Fitting our algorithm
+algorithm.fit(trainPredictors,trainingTarget)
+
+	#Testing on our test fold
+testPredictions = algorithm.predict(trainingSet[features].iloc[testIndices,:])
+predictions.append(testPredictions)
+print(predictions)
